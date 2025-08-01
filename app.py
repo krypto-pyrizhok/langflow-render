d51b2290fb6f903072a2b702886ecc4cb6a57d6b
Nazar <61500548+krypto-pyrizhok@users.noreply.github.com>
2025-08-02 01:11:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from langflow.main import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
+    # Цей блок для локального тестування, Gunicorn/uvicorn його ігнорують
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 7860)))
